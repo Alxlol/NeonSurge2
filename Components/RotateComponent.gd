9bbf6fc0a2cc7ac519f7006691f04b12302e7256
Alexander <friberg.alx@gmail.com>
2024-01-10 23:24:59 +0100

    Restructure folders, refactor into components

---
+++ Components/RotateComponent.gd
@@ -0,0 +1,11 @@
+class_name RotateComponent
+extends Node
+
+@export var sprite : Sprite2D
+@export var rotate_speed : float = 0.8
+
+func _physics_process(delta):
+	sprite.rotate(rotate_speed * delta)
+	
+	if Input.is_action_just_pressed("Rotate"):
+		sprite.rotate(rotate_speed)
---
+++ Components/ThrustComponent.gd
@@ -0,0 +1,12 @@
+class_name ThrustComponent
+extends Node
+
+@export var sprite : Sprite2D
+@export var physics_body : RigidBody2D
+@export var thrust_speed : float = 650
+
+func _process(delta):
+	if Input.is_action_just_pressed("Thrust"):
+		var sprite_forward_vector = Vector2(0, -1).rotated(sprite.rotation).normalized()
+		physics_body.apply_central_impulse(sprite_forward_vector * thrust_speed)
+
