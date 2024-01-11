f45d211b1ce8e5d984eca682c6d4a41ef0e04959
Alexander <friberg.alx@gmail.com>
2024-01-11 03:51:55 +0100

    more stuff

--- Actors/Characters/Player/Player.gd
+++ Actors/Characters/Player/Player.gd
@@ -1,6 +1,12 @@
 extends RigidBody2D
 
-@onready var start_position = global_transform.origin
+@onready var start_position : Vector2 = transform.get_origin()
+@export var max_health : int = 3
 
-func death():
+func takeDamage():
+	max_health -= 1
 	global_transform.origin = start_position
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	takeDamage()
--- Actors/DeathZone.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-
-func _on_area_2d_body_exited(body):
-	print(body)
-	if body == "res://Actors/Characters/Player/Player.tscn":
-		print('hello')
