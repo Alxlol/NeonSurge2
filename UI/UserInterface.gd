401ef018076a20aef0adac8684438799b2823f2c
Alexander <friberg.alx@gmail.com>
2024-12-22 15:54:57 +0100

    new pickup sprite

--- UI/UserInterface.gd
+++ UI/UserInterface.gd
@@ -8,12 +8,8 @@ func _ready():
 func update_health(new_health):
 	health.text = str(new_health)
 
-
-
-
 func _on_thrust_button_pressed() -> void:
 	PlayerState.player_instance.thrust_component.thrust_forward()
-
 
 func _on_rotate_button_pressed() -> void:
 	PlayerState.player_instance.rotate_component.rotate_player()
