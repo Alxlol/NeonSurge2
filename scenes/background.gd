0c6fd23e5b606972c9637f8a182855eae721061b
Alexander <friberg.alx@gmail.com>
2024-12-23 15:41:26 +0100

    infinit scrolling stars, random positioned background

---
+++ scenes/background.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var star_scroll_speed : float = 0.008
+var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_random_background_position()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$ParallaxBackground.scroll_offset.y += star_scroll_speed
+	pass
+
+func set_random_background_position() -> void:
+	$BG.position.x = rng.randi_range(0, -837)
+	$BG.position.y = rng.randi_range(0, -780)
