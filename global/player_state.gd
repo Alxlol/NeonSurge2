08ffb906536b1501c2d6dd50e67eef4e19ea0c2f
Alexander <friberg.alx@gmail.com>
2024-12-23 14:22:35 +0100

    renamed and restructured project

---
+++ global/player_state.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@onready var player_class : PackedScene = preload("res://scenes/player.tscn")
+var player_instance : Player
+
+var max_health: int = 3
+var current_health: int = 3
+
+signal set_new_health(new_health: int)
+
+func decrement_health(amount):
+	current_health -= amount
+	set_new_health.emit(current_health)
+
+func respawn_player(position:Vector2):
+	player_instance = player_class.instantiate()
+	player_instance.position = Vector2(position)
+	player_instance.name = "Player"
+	call_deferred('add_child', player_instance)
