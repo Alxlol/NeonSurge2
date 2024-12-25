4aba230f8fa921f1c370351819cb510f3bba7e2d
Alexander <friberg.alx@gmail.com>
2024-12-25 21:15:21 +0100

    module spawner

--- levels/infinity/module_spawner.gd
+++ levels/infinity/module_spawner.gd
@@ -1,11 +1,30 @@
 extends Node2D
 
+@export var scrolling_speed : float = 0.07
+var modules : Array[PackedScene] = [
+	preload("res://levels/infinity/modules/module_01.tscn"),
+	preload("res://levels/infinity/modules/module_02.tscn"),
+	preload("res://levels/infinity/modules/module_03.tscn"),
+]
 
-# Called when the node enters the scene tree for the first time.
+var index : int = 0
+
+func spawnNewModule():
+	index += 1
+	var first_module = modules[0].instantiate()
+	add_child(first_module)
+
 func _ready() -> void:
+	spawnNewModule()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	$".".position.y += scrolling_speed
+	print($".".position.y)
+	if ($".".position.y >= 800):
+		spawnNewModule()
+		print('spawning new module')
+	
 	pass
