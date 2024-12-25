4e7dc1de48db9cb42874fa5b4f5582b571ee6d37
Alexander <friberg.alx@gmail.com>
2024-12-25 23:29:30 +0100

    more module spawning

--- levels/infinity/module_spawner.gd
+++ levels/infinity/module_spawner.gd
@@ -7,24 +7,24 @@ var modules : Array[PackedScene] = [
 	preload("res://levels/infinity/modules/module_03.tscn"),
 ]
 
-var index : int = 0
-
-func spawnNewModule():
-	index += 1
-	var first_module = modules[0].instantiate()
-	add_child(first_module)
-
-func _ready() -> void:
-	spawnNewModule()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	$".".position.y += scrolling_speed
-	print($".".position.y)
-	if ($".".position.y >= 800):
-		spawnNewModule()
-		print('spawning new module')
-	
-	pass
+#var index : int = 0
+#
+#func spawnNewModule():
+	#index += 1
+	#var first_module = modules[0].instantiate()
+	#add_child(first_module)
+#
+#func _ready() -> void:
+	#spawnNewModule()
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#$".".position.y += scrolling_speed
+	#print($".".position.y)
+	#if ($".".position.y >= 800):
+		#spawnNewModule()
+		#print('spawning new module')
+	#
+	#pass
--- levels/infinity/modules/master_module.gd
+++ levels/infinity/modules/master_module.gd
@@ -1,6 +1,13 @@
 extends Node2D
-@export var scrolling_speed : float = 0.01
+@export var scrolling_speed : float = 0.1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _process(delta: float) -> void:
+	$".".position.y += scrolling_speed
+	if ($".".position.y >= 800):
+		print('should delete module')
+		queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body)
 	pass # Replace with function body.
